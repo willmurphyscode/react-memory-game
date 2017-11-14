d24d6e9bc9f4940f541e3a252a3958bc45f944d7
Will Murphy <willmurphyscode@gmail.com>
2017-11-14 18:46:07 -0500

    no longer need for stateful card

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,27 +1,11 @@
 import React from 'react';
 
-class Card extends React.Component {
-
-    constructor() {
-        super();
-    
-        this.state = {
-          faceUp: false
-        }
-    }
-
-    render() {
-        //console.log(this.state);
-        return    (
-            <div className={'card ' + (this.props.faceUp ? 'front' : 'back')} id={this.props.cardIx}>
-                { this.props.faceUp ? 'A' : '?'}
-            </div>
-        )
-    }
-}
+const card = (props) => (
+    <div className={'card ' + (props.faceUp ? 'front' : 'back')} id={props.cardIx}>
+        { props.faceUp ? 'A' : '?'}
+    </div>
+)
 
 
 
-
-
-export default Card;
+export default card;

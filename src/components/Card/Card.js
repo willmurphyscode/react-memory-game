9c64c4edf03d266af4dcb86825aca4c0d55c82b0
Will Murphy <willmurphyscode@gmail.com>
2017-11-14 18:07:54 -0500

    needs a constructor

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,10 +2,18 @@ import React from 'react';
 
 class Card extends React.Component {
 
+    constructor() {
+        super();
+    
+        this.state = {
+          faceUp: false
+        }
+    }
+
     render() {
         return    (
-            <div className={'card ' + (this.props.faceUp ? 'front' : 'back')} id={this.props.cardIx}>
-                { this.props.faceUp ? 'A' : '?'}
+            <div className={'card ' + (this.state.faceUp ? 'front' : 'back')} id={this.props.cardIx}>
+                { this.state.faceUp ? 'A' : '?'}
             </div>
         )
     }

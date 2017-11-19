671c0c9f89efe8eac8ee5c63837b9553f97d9a62
Will Murphy <willmurphyscode@gmail.com>
2017-11-19 09:39:34 -0500

    fix linting issues in App.spec

--- src/App.spec.js
+++ src/App.spec.js
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import Card from './components/Card';
 
-const Enzyme = require('enzyme');
 import Adapter from 'enzyme-adapter-react-16';
 import { shallow, mount } from 'enzyme';
+
+const Enzyme = require('enzyme');
+
 
 describe('The set of cards', () => {
   beforeAll(() => {
@@ -27,7 +29,6 @@ describe('The set of cards', () => {
   it('flips cards when you click them', () => {
     spyOn(App.prototype, 'flipCardByIx');
     const wrapper = shallow(<App rows={6} columns={8}/>);
-    const firstCard = wrapper.find(Card).first();
     wrapper.simulate('click', { target: { id: 0 }});
     expect(App.prototype.flipCardByIx).toHaveBeenCalled();
   });
@@ -35,7 +36,6 @@ describe('The set of cards', () => {
   //reimplement the above test without knowing function names
   it('makes a face up card when you click one', () => {
     const wrapper = mount(<App rows={6} columns={8}/>);
-    const firstCard = wrapper.find(Card).first();
     wrapper.simulate('click', { target: { id: 0 }});
     expect(wrapper.find('div.card.front')).toHaveLength(1);
   });

2ca1dbd97806705924fc84231194cdb7bef3a5a2
Will Murphy <willmurphyscode@gmail.com>
2017-11-23 06:45:00 -0500

    start building a scoreboard

---
+++ src/components/Scoreboard/Scoreboard.spec.js
@@ -0,0 +1,9 @@
+import Scoreboard from './Scoreboard';
+
+describe('scoreboard', () => {
+    
+    it('should render without crashing', () => {
+        expect(true).toBe(false);
+    });
+    
+})

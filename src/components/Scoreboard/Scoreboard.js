fa2e3242b92f0cbedfcaf0007ea797639b4de8db
Will Murphy <willmurphyscode@gmail.com>
2017-11-23 06:48:06 -0500

    very simple test

--- src/components/Scoreboard/Scoreboard.js
+++ src/components/Scoreboard/Scoreboard.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 class Scoreboard extends React.Component {
     render() {
-        return(<div></div>);
+        return(<div>
+            I'm a little scoreboard
+        </div>);
     }
 }
 
--- src/components/Scoreboard/Scoreboard.spec.js
+++ src/components/Scoreboard/Scoreboard.spec.js
@@ -1,9 +1,20 @@
+import React from 'react';
+
 import Scoreboard from './Scoreboard';
 
+import Adapter from 'enzyme-adapter-react-16';
+import { shallow, mount } from 'enzyme';
+
+const Enzyme = require('enzyme');
+
 describe('scoreboard', () => {
+    beforeAll(() => {
+        Enzyme.configure({ adapter: new Adapter() })
+      });
     
     it('should render without crashing', () => {
-        expect(true).toBe(false);
+        const wrapper = shallow(<Scoreboard />);
+        expect(wrapper.find('div')).toHaveLength(1);
     });
     
 })
